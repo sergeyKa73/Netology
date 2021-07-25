95274a94e951da5e8d79fa1e9f89d36f3e7b5daa
Sergey <karandeev73@gmail.com>
2021-07-25 17:55:26 +0300

    feat: add homeworks_4

---
+++ homeworks_4/pyfreetodobot.py
@@ -0,0 +1,19 @@
+import telebot
+
+token = '1804065816:AAE0-UXoZG9kRqWa53D97Gii3DcLqov3rJk'
+
+bot = telebot.TeleBot(token)
+
+my_name = 'Sergey'
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    if my_name in message.text:
+        text = 'Ба! Знакомые все лица'
+    else:
+        text = message.text
+    bot.send_message(message.chat.id, text)
+
+
+bot.polling(none_stop=True)
